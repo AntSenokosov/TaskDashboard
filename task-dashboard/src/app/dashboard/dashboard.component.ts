c83680189e8b432ccbe0653a521f96599b4e4d05
Anton Senokosov <a.y.senokosov@gmail.com>
2025-01-11 17:11:42 +0200

    small update

--- task-dashboard/src/app/dashboard/dashboard.component.ts
+++ task-dashboard/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IDashboardModel } from './dashboard.model';
-import { GridsterConfig, GridsterModule } from 'angular-gridster2'
+import { CompactType, GridsterConfig, GridsterModule, GridType } from 'angular-gridster2'
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -13,12 +13,12 @@ import { CommonModule } from '@angular/common';
 export class DashboardComponent {
   dashboard = [
     { type: 'chart', id: 1, x: 0, y: 0, rows: 2, cols: 3 },
-    { type: 'chart', id: 2, x: 3, y: 0, rows: 2, cols: 3 }
+    { type: 'chart', id: 2, x: 3, y: 0, rows: 2, cols: 3 },
   ];
 
   options: GridsterConfig = {
-    gridType: 'fit',
-    compactType: 'compactUp',
+    gridType: GridType.Fit,
+    compactType: CompactType.None,
     itemChangeCallback: (item) => console.log(item),
     itemResizeCallback: (item) => console.log(item)
   };
