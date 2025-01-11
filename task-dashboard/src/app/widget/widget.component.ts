a9ba18f8c81c3222865f84123c036136feec2eb5
Anton Senokosov <a.y.senokosov@gmail.com>
2025-01-12 01:43:29 +0200

    Update widget

--- task-dashboard/src/app/widget/widget.component.ts
+++ task-dashboard/src/app/widget/widget.component.ts
@@ -1,41 +1,41 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
-import { ChartOptions, ChartType, ChartData } from 'chart.js';
 import { FormsModule } from '@angular/forms';
-import { BaseChartDirective } from 'ng2-charts';
+import { CanvasJSAngularChartsModule } from '@canvasjs/angular-charts';
 
 @Component({
   selector: 'app-widget',
   standalone: true,
-  imports: [CommonModule, FormsModule, BaseChartDirective ],
+  imports: [CommonModule, FormsModule, CanvasJSAngularChartsModule],
   templateUrl: './widget.component.html',
   styleUrl: './widget.component.css'
 })
 export class WidgetComponent implements OnInit {
-  @Input() type: string = ''; 
   @Input() widget: any;
-  chartType: ChartType = 'line';
-  chartOptions: ChartOptions = {
-    responsive: true,
-  };
 
-  chartData: ChartData<'line'> = {
-    labels: ['January', 'February', 'March', 'April', 'May'],
-    datasets: [
-      {
-        label: 'Sales',
-        data: [10, 20, 30, 40, 50],
-        borderColor: 'blue',
-        fill: false
-      }
-    ]
-  };
+  chartOptions: any;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-    if (this.type === 'chart') {
-      console.log('Chart widget initialized');
-    }
+    this.initializeChart();
+  }
+
+  initializeChart(): void {
+    this.chartOptions = {
+      title: {
+        text: "Basic Column Chart in Angular"
+      },
+      data: [{
+        type: "column",
+        dataPoints: [
+          { label: "Apple",  y: 10  },
+          { label: "Orange", y: 15  },
+          { label: "Banana", y: 25  },
+          { label: "Mango",  y: 30  },
+          { label: "Grape",  y: 28  }
+        ]
+      }]                
+    };
   }
 }
